d3e4859e1fb560ffa3925e3ffd0c25a6f0e6a020
Sean Keever <33592180+swkeever@users.noreply.github.com>
2026-08-30 12:39:49 -0400

    test(auth): require refreshed access tokens

--- features/step_definitions/sdk_contract_steps.rb
+++ features/step_definitions/sdk_contract_steps.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+ACCESS_TOKEN_CLOCK_TICK_SECONDS = 1.1
+
 def contract
   raise 'contract world is not initialized' unless @contract
 
@@ -37,6 +39,7 @@ When('the client refreshes the current session') do
   contract.previous_session = contract.client.auth.current_session
   raise 'current session is missing' unless contract.previous_session
 
+  sleep ACCESS_TOKEN_CLOCK_TICK_SECONDS
   contract.record { contract.client.auth.refresh_session }
 end
 
@@ -60,6 +63,7 @@ end
 Then('the refreshed session becomes current') do
   refreshed = contract.last_outcome.value
   raise 'refresh returned the previous session' if refreshed.equal?(contract.previous_session)
+  raise 'refresh did not issue a new access token' if refreshed.access_token == contract.previous_session.access_token
   raise 'refreshed session is not current' unless contract.client.auth.current_session.equal?(refreshed)
 end
 

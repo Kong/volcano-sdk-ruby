f94c5e16bf1b3c6c89b95b8566262004b13c5ad2
Sean Keever <33592180+swkeever@users.noreply.github.com>
2026-09-18 15:21:38 -0400

    test(realtime): cover native channel cleanup compatibility

--- spec/volcano_contract/presence_membership_spec.rb
+++ spec/volcano_contract/presence_membership_spec.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
 require_relative '../../features/support/presence_membership'
+require_relative '../../features/support/contract_world'
 
 RSpec.describe VolcanoContract::PresenceMembership do
+  it 'cleans up using the native channel API' do
+    clients = Array.new(2) { Volcano::Client.new(anon_key: 'anon', api_url: 'https://api.test') }
+    world = instance_double(VolcanoContract::World, realtime_clients: clients,
+                                                    realtime_channel: 'lobby', fixture: { 'user_id' => 'user' })
+    verifier = described_class.new(world)
+    allow(verifier).to receive(:check_membership).and_return([1, 2, 1])
+
+    expect(Async { |task| verifier.run(task) }.wait).to eq([1, 2, 1])
+  ensure
+    Async { clients&.each { |client| client.realtime.disconnect } }.wait
+  end
+
   [
     [[['first'], %w[first second], ['first']], true],
     [[['first'], %w[first second]], false],

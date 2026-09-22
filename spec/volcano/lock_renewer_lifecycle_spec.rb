d415d110841b8fa26e60e035ed243e1be0b606be
Sean Keever <33592180+swkeever@users.noreply.github.com>
2026-09-21 21:57:59 -0400

    refactor: require a remaining window for lock renewal

--- spec/volcano/lock_renewer_lifecycle_spec.rb
+++ spec/volcano/lock_renewer_lifecycle_spec.rb
@@ -23,6 +23,25 @@ module Volcano
       expect(locks).not_to have_received(:renew)
     end
 
+    it 'does not renew when ownership is lost while waiting to renew' do
+      observed_loss = Queue.new
+      failure = RuntimeError.new('ownership lost while waiting')
+      allow(guard).to receive(:renewal_delay) do
+        guard.mark_lost(failure)
+        0
+      end
+      allow(guard).to receive(:lost?).and_wrap_original do |original|
+        original.call.tap { |lost| observed_loss << true if lost }
+      end
+      renewer.start
+      Timeout.timeout(1) { observed_loss.pop }
+
+      expect(locks).not_to have_received(:renew)
+      expect(guard.failure).to equal(failure)
+    ensure
+      renewer.stop
+    end
+
     it 'does not schedule or send a renewal for an already lost lease' do
       failure = RuntimeError.new('ownership lost')
       guard.mark_lost(failure)

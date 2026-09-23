9da7b90ccaba04f1b08486a7f983cc40feefee71
Sean Keever <33592180+swkeever@users.noreply.github.com>
2026-09-23 01:44:54 -0400

    build(types): check log responses with Steep

--- lib/volcano/log_responses.rb
+++ lib/volcano/log_responses.rb
@@ -20,24 +20,28 @@ module Volcano
   end
   private_constant :ImmutableLogJSON
 
+  LogSearchResponse = Data.define(:data, :limit, :has_more, :next_cursor)
+
   # Immutable page returned by a project log search.
-  LogSearchResponse = Data.define(:data, :limit, :has_more, :next_cursor) do
+  class LogSearchResponse
     include ImmutableLogJSON
 
     def initialize(data:, limit:, has_more:, next_cursor: nil)
-      super(
-        data: immutable_json(data), limit: limit, has_more: has_more,
-        next_cursor: next_cursor&.dup&.freeze
-      )
+      data = immutable_json(data)
+      next_cursor = next_cursor&.dup&.freeze
+      super
     end
   end
 
+  LogActivityResponse = Data.define(:data, :total)
+
   # Immutable bucketed project log activity.
-  LogActivityResponse = Data.define(:data, :total) do
+  class LogActivityResponse
     include ImmutableLogJSON
 
     def initialize(data:, total:)
-      super(data: immutable_json(data), total: total)
+      data = immutable_json(data)
+      super
     end
   end
 end
